91c34199e0554c5e89c79cc021da4a2a084771ff
Pedro Pagan <pedroantoniobonaldo@gmail.com>
2026-02-02 00:26:15 -0300

    cleanup

--- v1_taskId_page/$taskId/-components/index.ts
+++
@@ -1,5 +0,0 @@
-export { ManageTaskForm } from "./ManageTaskForm";
-export { ManageTaskHeader } from "./ManageTaskHeader";
-export { ManageTaskMetadata } from "./ManageTaskMetadata";
-export { ManageTaskRoot } from "./ManageTaskRoot";
-export { ManageTaskStatus } from "./ManageTaskStatus";
